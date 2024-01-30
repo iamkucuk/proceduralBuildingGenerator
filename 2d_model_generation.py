68d9d529e4b08d76b58048b6ad6c4ec8264b1a19
Furkan Küçük <furkankucuk@outlook.com>
2024-01-30 16:54:02 +0300

    Initial commit for 2D model generation

---
+++ 2d_model_generation.py
@@ -0,0 +1,14 @@
+# This is the initial version of the 2 D model generation script.
+
+#python libraries
+import numpy as np
+import math
+miport random
+import sys
+import os
+import csv
+from copy import copy
+from statistics import mean
+
+# The below code will be adapted from the current main_generateBuilding.py script
+# The objective is to generate a 2 D representation of the building plan without any Blender dependencies.
